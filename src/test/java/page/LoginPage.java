94518c6e1bce1988667c621d1f45f26cca0e6c2d
LENOVO <LENOVO@DESKTOP-LE119FM>
2023-06-16 20:28:09 +0530

    Created ETT POM Class

--- src/test/java/page/LoginPage.java
+++ src/test/java/page/LoginPage.java
@@ -1,5 +1,40 @@
 package page;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class LoginPage {
 
+	@FindBy(id="username")
+	private WebElement unTB;
+	
+	@FindBy(name="pwd")
+	private WebElement PwdTB;
+	
+	@FindBy(xpath="//a[@id='loginButton']")
+	private WebElement loginBTN;
+	
+	public LoginPage(WebDriver driver)
+	{
+		PageFactory.initElements(driver, this);
+	}
+	
+	public void setUserName(String un)
+	{
+		unTB.sendKeys(un);
+	}
+	
+	public void setPassword(String pwd)
+	{
+		PwdTB.sendKeys(pwd);
+	}
+    
+	public void clickLoginButton()
+	{
+		loginBTN.click();
+	}
+	
+	
 }
